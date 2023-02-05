b9e28bda0b1e72bb3e1ead6903166382c2d75658
imamhosen737 <imamhosen737@gmail.com>
2023-02-06 02:50:08 +0600

    modify

--- Assignment2.php
+++ Assignment2.php
@@ -4,7 +4,7 @@ $fee = 18500;    //User's input
 $twenty_five_percent = $fee * 25 / 100;  //Total of 25 parcent commission 
 $twenty_percent = $fee * 20 / 100;      //Total of 20 parcent commission 
 $fifteen_percent = $fee * 15 / 100;     //Total of 15 parcent commission 
-$invalid = "Invalid";                  //For tution fee less than 7000
+$invalid = "Invalid";                  //If tution fee is less than 7000
 
 //Logic
 $commission = ($fee >= 20000) ? ($twenty_five_percent) : (($fee > 10000 && $fee < 20000) ? ($twenty_percent) : (($fee < 10000 && $fee > 7000) ? ($fifteen_percent) : $invalid));
